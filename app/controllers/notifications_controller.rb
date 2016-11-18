6afaf783d5fa290570da34662a563ddfd93c9004
huyliver6793 <huynd6793@gmail.com>
2016-11-18 11:20:13 +0700

    refactor_rest

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,12 +8,4 @@ class NotificationsController < ApplicationController
     @notification = Notification.find_by id: params[:id]
     @notification.update_attributes status: true
   end
-
-  def read_noti_all
-    Notification.by_receiver(current_user.id).unread.update_all status: true
-    respond_to do |format|
-      format.html {redirect_to notifications_path}
-      format.js
-    end
-  end
 end
---
+++ app/controllers/reads_controller.rb
@@ -0,0 +1,7 @@
+class ReadsController < ApplicationController
+  def update
+    Notification.by_receiver(current_user.id).unread.update_all status: true
+    respond_to do |format|
+      format.js
+  end
+end
