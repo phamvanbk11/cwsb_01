79e94c3c1a782f02cfcb3dcbd35e62c5a110b833
ledinhdoanframgia <le.dinh.doan@framgia.com>
2016-11-30 17:31:31 +0700

    confirm_payment_directly

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -4,4 +4,9 @@ class BookingMailer < ApplicationMailer
     @state = state
     mail to: email, subject: t("bookings.email")
   end
+
+  def change_status_payment_directly status, email
+    @status = status
+    mail to: email, subject: t("payments.directly.email")
+  end
 end
